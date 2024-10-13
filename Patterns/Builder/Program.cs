6869ceab99ae22c839bca646c6d11617a2d86ce5
Alexey <paaworker@gmail.com>
2024-10-13 13:08:54 +0300

    add builder

---
+++ Patterns/Builder/Program.cs
@@ -0,0 +1,52 @@
+﻿abstract class Builder
+{
+    public abstract void BuilderPartA();
+    public abstract void BuilderPartB();
+    public abstract void BuilderPartC();
+    public abstract Product GetResult();
+}
+
+internal class Product
+{
+    public List<string> list = new();
+
+    public void Add(string part) => list.Add(part);
+}
+
+internal class Director
+{
+    private Builder Builder;
+
+    public Director(Builder builder) => this.Builder = builder;
+
+    public void Construct()
+    {
+        Builder.BuilderPartA();
+        Builder.BuilderPartB();
+        Builder.BuilderPartC();
+    }
+}
+
+internal class ConcreteBuilder : Builder
+{
+    private Product Product = new();
+
+    public override void BuilderPartA() => Product.Add("part_1");
+    public override void BuilderPartB() => Product.Add("part_2");
+    public override void BuilderPartC() => Product.Add("part_3");
+
+    public override Product GetResult() => Product;
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = new ConcreteBuilder();
+        var director = new Director(builder);
+
+        director.Construct();
+
+        builder.GetResult().list.ForEach(x => Console.WriteLine(x));
+    }
+}
