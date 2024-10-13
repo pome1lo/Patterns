9137dbfd63a4d3aec580fa79e1b81342cf979db5
Alexey <paaworker@gmail.com>
2024-10-13 12:56:22 +0300

    add .gitignore / create project / add singleton

---
+++ Patterns/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Singleton
+{
+    private Singleton() { }
+
+    private static Singleton _instance = null!;
+
+    public static Singleton GetInstance()
+    {
+        _instance ??= new Singleton();
+        return _instance;
+    }
+}
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        var singleton = Singleton.GetInstance();
+
+        Console.WriteLine(singleton.ToString());
+    }
+}
